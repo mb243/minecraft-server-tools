de2189a1b829778decc69972a688e6e9aaa23e28
Mike <mb243@users.noreply.github.com>
2017-04-09 07:51:26 -0400

    Create build.sh

---
+++ cron/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rev="1.11.2"
+
+# clean	the build env
+rm -rf /tmp/spigotbuilder && \
+mkdir -p /tmp/spigotbuilder && \
+cd /tmp/spigotbuilder
+
+#added for overviewer compatibility
+rm -f ~/.minecraft/versions/${rev}/${rev}.jar
+wget https://s3.amazonaws.com/Minecraft.Download/versions/${rev}/${rev}.jar -P ~/.minecraft/versions/${rev}/
+
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar && \
+java -jar BuildTools.jar --rev $rev
+
+wget http://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
+
+rm -f craftbukkit-*.jar
