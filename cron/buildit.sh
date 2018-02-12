575bbeb2d6f549be12de635a02bb784cc856e24b
Mike <9483554+mb243@users.noreply.github.com>
2018-02-11 20:41:59 -0500

    Don't rely on wget

--- cron/buildit.sh
+++ cron/buildit.sh
@@ -9,17 +9,18 @@ jardir="/home/minecraft/MinecraftServer/jar/"
 
 # set up the build env
 cd $tmpdir
-wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+curl -o BuildTools.jar -L https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 
 # start building
 java -jar BuildTools.jar --rev $rev
 
 #added for overviewer compatibility
 rm -f ~/.minecraft/versions/${rev}/${rev}.jar
-wget https://s3.amazonaws.com/Minecraft.Download/versions/${rev}/${rev}.jar -P ~/.minecraft/versions/${rev}/
+curl -o ${rev}.jar -L https://s3.amazonaws.com/Minecraft.Download/versions/${rev}/${rev}.jar
+mv -f ${rev}.jar ~/.minecraft/versions/${rev}/
 
 # added for bungeecord
-#wget http://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
+#curl -o BungeeCord.jar -L http://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
 #cp -f BungeeCord.jar ~/jar/bungeecord.jar
 
 # get the name of the jar file
